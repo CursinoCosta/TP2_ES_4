de75824f121dd6847db28d77d504fccf891a2625
CursinoCosta] <mateuscgcosta@gmail.com>
2025-06-05 20:25:40 -0300

    +3 testes adicionados

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,105 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Testa botão marcar todos como feito' , () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Comprar pão{enter}')
+      .type('Estudar Cypress{enter}')
+      .type('Fazer exercícios{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('.toggle-all-label')
+      .click();
+
+    cy.get('[data-cy=filter-completed-link]')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('[data-cy=filter-active-link]')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+
+    cy.get('.toggle-all-label')
+      .click();
+
+    cy.get('[data-cy=filter-completed-link]')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+
+    cy.get('[data-cy=filter-active-link]')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 3);
+  });
+
+it('Testa botão clear completed', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('.toggle-all-label')
+    .click();
+    
+
+    cy.get('.clear-completed').
+    click();
+
+    cy.get('[data-cy=todo-input]')
+      .type('Comprar pão{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+  });
+
+  it('Cria e edita uma tarefa', ()=>{
+    cy.visit('');
+
+    const tarefaOriginal = 'Comprar leite';
+    const tarefaEditada = 'Comprar café';
+
+    cy.get('[data-cy=todo-input]')
+      .type(`${tarefaOriginal}{enter}`);
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', tarefaOriginal);
+
+    cy.get('[data-cy=todos-list] li label')
+      .first()
+      .dblclick();
+
+    cy.get('.edit')
+      .should('have.value', tarefaOriginal)
+      .clear()
+      .type(`${tarefaEditada}{enter}`);
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', tarefaEditada);
+  });
+
 });
